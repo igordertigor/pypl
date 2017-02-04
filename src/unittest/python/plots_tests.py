e0726ed94144da8a74c7f8c125c5cfcb94a02953
Ingo Fründ <mail@ingofruend.net>
2017-02-04 16:45:36 +0100

    testing attr

--- src/unittest/python/plots_tests.py
+++ src/unittest/python/plots_tests.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest import mock
 
 from pypl import plots
 
@@ -11,3 +12,15 @@ class TestElementsCollection(unittest.TestCase):
         coll['ANY_OTHER_GROUP'].append('ANY_OTHER_PLOT_ELEMENT')
         self.assertSetEqual(set(coll.elements),
                             {'ANY_PLOT_ELEMENT', 'ANY_OTHER_PLOT_ELEMENT'})
+
+    def test_attr_should_be_chainable(self):
+        mock_svg_element = mock.Mock()
+        mock_svg_element.attribs = {}
+
+        coll = plots.ElementsCollection()
+        coll['ANY_GROUP'].append(mock_svg_element)
+
+        coll.attr('ANY_ATTRIBUTE', 2)
+
+        self.assertIn('ANY_ATTRIBUTE', mock_svg_element.attribs)
+        self.assertEqual(mock_svg_element.attribs['ANY_ATTRIBUTE'], 2)
