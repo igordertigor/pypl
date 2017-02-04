50d66b5a732abcf0dd3693ae6ddf3e5258a79e83
Ingo Fründ <mail@ingofruend.net>
2017-02-04 17:09:00 +0100

    testing scatter plot

--- src/unittest/python/plots_tests.py
+++ src/unittest/python/plots_tests.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest import mock
 
+import svgwrite
+
 from pypl import plots
 
 
@@ -24,3 +26,22 @@ class TestElementsCollection(unittest.TestCase):
 
         self.assertIn('ANY_ATTRIBUTE', mock_svg_element.attribs)
         self.assertEqual(mock_svg_element.attribs['ANY_ATTRIBUTE'], 2)
+
+
+class TestScatterPlot(unittest.TestCase):
+
+    def test_should_create_circle_elements_per_data_point(self):
+        coll = plots.scatterplot([0], [1, 2], ['#000'])
+
+        points = [e for e in coll['points']
+                  if isinstance(e, svgwrite.shapes.Circle)]
+        self.assertEqual(len(points), 2)
+
+    def test_should_add_correct_colors_to_circles(self):
+        coll = plots.scatterplot([0], [1, 2], ['#000', '#e2a'])
+
+        pointcolors = [e.attribs['fill'] for e in coll['points']
+                       if isinstance(e, svgwrite.shapes.Circle)]
+
+        self.assertSetEqual(set(pointcolors),
+                            {'#000', '#e2a'})
