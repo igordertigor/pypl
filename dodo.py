fbd95719166d8fc60f3b785adc14d082fac9ae71
Ingo Fründ <mail@ingofruend.net>
2019-11-17 17:08:54 -0500

    dependency installation

--- dodo.py
+++ dodo.py
@@ -1,5 +1,31 @@
+def update(d, info):
+    d.update(info)
+    return d
+
+
 def task_test():
     return {
         'actions': ['PYTHONPATH=. py.test tests/*_tests.py'],
         'verbosity': 2,
+    }
+
+
+def task_develop():
+    yield update(
+        task_install_dev_dependencies(),
+        {'name': 'install_dev_dependencies'})
+    yield update(
+        task_install_runtime_dependencies(),
+        {'name': 'install_runtime_dependences'})
+
+
+def task_install_dev_dependencies():
+    return {
+        'actions': ['pip install -r requirements/dev.txt'],
+    }
+
+
+def task_install_runtime_dependencies():
+    return {
+        'actions': ['pip install -r requirements/run.txt'],
     }
