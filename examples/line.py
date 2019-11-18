ad9b143b433c14b86aa9441333e49d06f12ca423
Ingo Fruend <mail@ingofruend.net>
2019-11-18 10:04:51 -0500

    adjusting example

--- examples/line.py
+++ examples/line.py
@@ -10,5 +10,7 @@ css = '''
 fig = pypl.Figure()
 fig.append(pypl.InlineCss(css))
 # fig.append(pypl.ExternalCss('style.css'))
-fig.append(pypl.Line([10, 40, 100], [20, 300, 50], id_='myline'))
+fig.append(pypl.Line([10, 40, 100], [20, 300, 50],
+                     id_='myline',
+                     class_='condition1'))
 print(fig.render(pretty=True))
