97b56cd5d59145791570dca5b219806d7f93c9c8
Ingo Fruend <mail@ingofruend.net>
2019-11-19 15:15:10 -0500

    axes example

--- examples/axes.py
+++ examples/axes.py
@@ -1,4 +1,5 @@
 import pypl
+from random import normalvariate
 
 css = '''
 .line-plot {
@@ -8,11 +9,37 @@ css = '''
 .axis {
   stroke: black;
 }
+
+.dot-plot-marker {
+  stroke: white;
+  fill: #edab88;
+}
+
+.ticklabel {
+  stroke: none;
+}
+
+.xticklabel {
+  text-anchor: middle;
+  dominant-baseline: hanging;
+}
+
+.yticklabel {
+  text-anchor: end;
+  dominant-baseline: middle;
+}
 '''
+
+npoints = 100
+xv = [normalvariate(0, 1) for i in range(npoints)]
+yv = [0.1*xv[i] + normalvariate(0, 1) for i in range(npoints)]
 
 fig = pypl.Figure()
 fig.append(pypl.InlineCss(css))
-x = pypl.LinearAxis('x', [0, 1, 2], [400, 100], 100)
-x.ticksize = 20
-fig.append(x)
+x = pypl.LinearAxis('x', [-3, -1, 1, 3], [100, 300], 300)
+x.ticksize = 5
+y = pypl.LinearAxis('y', [-3, -1, 1, 3], [300, 100], 100)
+y.ticksize = -5
+fig.append([x, y])
+fig.append(pypl.Dots(x(xv), y(yv)))
 print(fig.render(pretty=True))
